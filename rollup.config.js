b3f404f744dd56394292a56487e3c2648b036bdf
Ffloriel <florielfedry@gmail.com>
2018-03-21 07:16:22 +0000

    initial setup project
    - rename test folder __tests__
    - add npmrc
    - add dependencies: flow, jest, rollup

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import babel from "rollup-plugin-babel"
+import builtins from "rollup-plugin-node-builtins"
+import resolve from "rollup-plugin-node-resolve"
+import flow from "rollup-plugin-flow"
+import uglify from "rollup-plugin-uglify"
+
+export default {
+    input: "src/index.js",
+    output: [
+        {
+            file: "lib/grunt-purgecss.es.js",
+            format: "es"
+        },
+        {
+            file: "lib/grunt-purgecss.js",
+            format: "cjs"
+        }
+    ],
+    plugins: [builtins(), resolve(), flow(), babel(), uglify()],
+    external: []
+}
