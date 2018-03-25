5b62abdaeb78ae9b6a0cfb162ae7ab4e0351ff42
Ffloriel <florielfedry@gmail.com>
2018-03-25 02:58:21 +0100

    v0.21.0
    - add lint with eslint
    - jest tests

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,16 @@
 import babel from "rollup-plugin-babel"
 import builtins from "rollup-plugin-node-builtins"
 import resolve from "rollup-plugin-node-resolve"
-import flow from "rollup-plugin-flow"
 import uglify from "rollup-plugin-uglify"
 
 export default {
     input: "src/index.js",
     output: [
-        // {
-        //     file: "lib/grunt-purgecss.es.js",
-        //     format: "es"
-        // },
         {
             file: "lib/grunt-purgecss.js",
             format: "cjs"
         }
     ],
-    plugins: [builtins(), resolve(), flow(), babel(), uglify()],
+    plugins: [builtins(), resolve(), babel(), uglify()],
     external: ['purgecss']
 }
